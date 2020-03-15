fc70bf4274873fd09bcc327a3f9ba03da3a0774d
czo <czodroid@gmail.com>
2020-03-15 05:08:30 +0100

    init

---
+++ showPassword.js
@@ -0,0 +1,68 @@
+// Filename: showPassword.js
+// Author_: a1618@love.com
+// Modified By: Olivier Sirol <czo@free.fr>
+// License: GPL-2.0
+// File Created: mars 2020
+// Last Modified: dimanche 15 mars 2020, 05:07
+// Edit Time: 1:13:26
+// Description:
+//      Extension for chromium and firefox
+//      Show password when double-clicking over password fields. 
+//      Same as https://chrome.google.com/webstore/detail/showpassword/bbiclfnbhommljbjcoelobnnnibemabl
+//      but only whith double-click.
+//      This is to be sure that no one steal my password over internet....
+//
+// $Id:$
+
+(function (win) {
+    'use strict';
+    var KEY_ENTER = 13;
+    var KEY_CTRL = 17;
+
+    function mouseDblClick(tar) {
+        tar.addEventListener('dblclick', function () {
+            if (tar.type === 'password') {
+                tar.type = 'text';
+            } else {
+                tar.type = 'password';
+            }
+        }, false);
+
+        tar.addEventListener('blur', function () {
+            tar.type = 'password';
+        }, false);
+
+        tar.addEventListener('keydown', function (e) {
+            if (e.keyCode === KEY_ENTER)
+                tar.type = 'password';
+        }, false);
+    }
+
+    var doc = win.document;
+    var MutationObserver = win.MutationObserver || win.WebKitMutationObserver;
+
+    function modifyInputs() {
+        var passwordInputs = doc.querySelectorAll('input[type=password]');
+        for (var j = 0; j < passwordInputs.length; j++) {
+            var passwordInput = passwordInputs[j];
+
+            if (!passwordInput.ready) {
+                mouseDblClick(passwordInput);
+                passwordInput.ready = true; //mark as modified
+            }
+        }
+    }
+
+    modifyInputs();
+
+    var observer = new MutationObserver(modifyInputs);
+
+    observer.observe(doc.documentElement, {
+        childList: true,
+        subtree: true
+    });
+
+})(this);
+
+
+
